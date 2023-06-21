8cb9b79f84203e2fb7c7d2f25f96e1db728e2717
Mateus <mateusppa10@gmail.com>
2023-06-21 09:15:06 -0300

    :construction: Reproduzindo bug

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,8 +1,8 @@
-import { revalidateTag } from 'next/cache';
+import { revalidatePath, revalidateTag } from 'next/cache';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET() {
   console.log('entrei aqui');
-  revalidateTag('clientes');
+  revalidatePath('/clientes');
   return NextResponse.json({ revalidated: true, now: Date.now() });
 }
